7c88a4072e80f3da56782836ebcacb7d1bd1b56a
Aboagye=Heming <hemingkwabena@gmail.com>
2023-03-03 00:32:41 +0000

    activted the toggle button

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let toggle = document.querySelector("#toggle");
 let monthly = document.querySelectorAll(".price-monthly");
-let yearly = document.querySelector(".price-annually");
+let yearly = document.querySelectorAll(".price-annually");
 
-toggle.addEventListener(click, function () {
-  if (toggle.checked) {
+toggle.addEventListener("change", function (e) {
+  if (e.target.checked) {
     yearly.forEach((price) => (price.style.display = "none"));
     monthly.forEach((price) => (price.style.display = "flex"));
   } else {
     yearly.forEach((price) => (price.style.display = "flex"));
-    monthly.forEach((price) => (price.style.display = "none"));
+    monthly.forEach((price) => (price.style.display = "none")); 
   }
 });
